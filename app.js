eddbba4ad8cb87f0d01e4450c2a99709b32c2d05
wushbin <wushbin@outlook.com>
2017-10-25 03:05:28 +0000

    to

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
-//mongoose.connect("mongodb://wushbin:3351567@ds231245.mlab.com:31245/webapp", {useMongoClient: true});
+//mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://wushbin:3351567@ds231245.mlab.com:31245/webapp", {useMongoClient: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
